8ed64ce3990f3a77f1efe25883259f2fc9121ca1
Ange-Cesari <contact@ange-cesari.com>
2025-04-08 02:05:17 +0200

    chore(lint): add commitlint configuration file

---
+++ .commitlintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert'
+      ]
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'core',
+        'model',
+        'prompt',
+        'generator',
+        'config',
+        'init',
+        'cli',
+        'docs',
+        'deps'
+      ]
+    ],
+    'scope-empty': [2, 'never'],
+    'subject-case': [0],
+    'body-max-line-length': [0]
+  }
+};
