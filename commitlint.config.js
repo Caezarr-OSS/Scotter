148463a763aafa0782b57d5378bb98bb5ffcc85b
Test User <test@example.com>
2025-06-30 00:43:28 +0200

    ci: remove windows from build matrix and adjust commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,11 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [1, 'always', 100],
+    // Allow capitalized subject (our current commits use capitalization)
+    'subject-case': [0, 'always', []],
+    // Make these warnings instead of errors
+    'type-enum': [1, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+    'subject-empty': [1, 'never'],
+    'type-empty': [1, 'never']
   },
 };
